0c96348e86de1a693b9808e6954785068461e5c2
Ruben Torres <rubentdlh@gmail.com>
2019-02-04 20:12:08 -0300

    Customize L&F

--- src/App.js
+++ src/App.js
@@ -9,22 +9,24 @@ class App extends Component {
       <div className="App">
         <Card style={{ width: 400 }}>
           <H5>Register</H5>
-          <FormGroup
-            label="First Name"
-          >
-            <InputGroup placeholder="First Name" />
-          </FormGroup>
-          <FormGroup
-            label="Last Name"
-          >
-            <InputGroup placeholder="Last Name" />
-          </FormGroup>
-          <FormGroup
-            label="Email"
-          >
-            <InputGroup type="email" placeholder="Email" />
-          </FormGroup>
-          <Button>Submit</Button>
+          <div className="card-content">
+            <FormGroup
+              label="First Name"
+            >
+              <InputGroup placeholder="First Name" />
+            </FormGroup>
+            <FormGroup
+              label="Last Name"
+            >
+              <InputGroup placeholder="Last Name" />
+            </FormGroup>
+            <FormGroup
+              label="Email"
+            >
+              <InputGroup type="email" placeholder="Email" />
+            </FormGroup>
+            <Button fill>Submit</Button>
+          </div>
         </Card>
       </div>
     );
