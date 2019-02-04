b9ab50bbe8bb8bf8babbf36cf50bb6a668c455ac
Ruben Torres <rubentdlh@gmail.com>
2019-02-04 20:00:26 -0300

    Add form layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import '@blueprintjs/core/lib/css/blueprint.css';
+import { Card, H5, FormGroup, InputGroup, Button } from '@blueprintjs/core';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Card style={{ width: 400 }}>
+          <H5>Register</H5>
+          <FormGroup
+            label="First Name"
+          >
+            <InputGroup placeholder="First Name" />
+          </FormGroup>
+          <FormGroup
+            label="Last Name"
+          >
+            <InputGroup placeholder="Last Name" />
+          </FormGroup>
+          <FormGroup
+            label="Email"
+          >
+            <InputGroup type="email" placeholder="Email" />
+          </FormGroup>
+          <Button>Submit</Button>
+        </Card>
       </div>
     );
   }
