b48f0eb1b9ecac72e09639070791d885f9a2c8d5
Ruben Torres <rubentdlh@gmail.com>
2019-02-04 21:27:43 -0300

    Tweak button styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import { Card, Form, Input, Button } from 'antd';
+import 'antd/dist/antd.css'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Card title="Register" style={{ width: 400 }}>
+          <Form>
+            <Form.Item label="First Name">
+              <Input placeholder="First Name" />
+            </Form.Item>
+            <Form.Item label="Last Name">
+              <Input placeholder="Last Name" />
+            </Form.Item>
+            <Form.Item label="Email">
+              <Input type="Email" placeholder="Email" />
+            </Form.Item>
+            <Button type="primary" block>
+              Submit
+            </Button>
+          </Form>
+        </Card>
       </div>
     );
   }
